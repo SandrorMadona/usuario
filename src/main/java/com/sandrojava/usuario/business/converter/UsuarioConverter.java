e589edab4723ed541d4053857deab542798ae49c
Sandro <sandrormadona@gmail.com>
2025-07-09 18:34:22 -0300

    adicionado método que salva dados usuario

---
+++ src/main/java/com/sandrojava/usuario/business/converter/UsuarioConverter.java
@@ -0,0 +1,99 @@
+package com.sandrojava.usuario.business.converter;
+
+import com.sandrojava.usuario.business.dto.EnderecoDTO;
+import com.sandrojava.usuario.business.dto.TelefoneDTO;
+import com.sandrojava.usuario.business.dto.UsuarioDTO;
+import com.sandrojava.usuario.infrastructure.entity.Endereco;
+import com.sandrojava.usuario.infrastructure.entity.Telefone;
+import com.sandrojava.usuario.infrastructure.entity.Usuario;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UsuarioConverter {
+
+    public Usuario paraUsuario(UsuarioDTO usuarioDTO){
+        return Usuario.builder()
+                .nome(usuarioDTO.getNome())
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListaEndereco(usuarioDTO.getEnderecos()))
+                .telefones(paraListaTelefones(usuarioDTO.getTelefones()))
+                .build();
+
+    }
+    public List<Endereco> paraListaEndereco(List<EnderecoDTO> enderecoDTOS){
+        List<Endereco> enderecos = new ArrayList<>();
+        for(EnderecoDTO enderecoDto : enderecoDTOS){ // um for para que transforme a funcao a baixo em uma lista
+            enderecos.add(paraEndereco(enderecoDto));
+        }
+        return enderecos;
+    }
+
+    public Endereco paraEndereco(EnderecoDTO enderecoDTO){
+        return Endereco.builder()
+                .rua(enderecoDTO.getRua())
+                .numero(enderecoDTO.getNumero())
+                .cidade(enderecoDTO.getCidade())
+                .complemento(enderecoDTO.getComplemento())
+                .cep(enderecoDTO.getCep())
+                .estado(enderecoDTO.getEstado())
+                .build();
+    }
+
+    public List<Telefone> paraListaTelefones(List<TelefoneDTO> telefoneDTOS){
+    return telefoneDTOS.stream().map(this::paraTelefone).toList(); //.stream.map faz o mesmo que o for, pegando cada item do telefone e retornando, ja o .toList pega esse retorno dos itens e joga na paraListTelefone como lista
+    }
+
+    public Telefone paraTelefone(TelefoneDTO telefoneDTO){
+        return Telefone.builder()
+                .numero(telefoneDTO.getNumero())
+                .ddd(telefoneDTO.getDdd())
+                .build();
+    }
+
+    // ----------------------------------------------------------------------------------------------------------------------------------
+    // build de conversao para DTO
+    public UsuarioDTO paraUsuarioDTO(Usuario usuarioDTO){
+        return UsuarioDTO.builder()
+                .nome(usuarioDTO.getNome())
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListaEnderecoDTO(usuarioDTO.getEnderecos()))
+                .telefones(paraListaTelefonesDTO(usuarioDTO.getTelefones()))
+                .build();
+
+    }
+    public List<EnderecoDTO> paraListaEnderecoDTO(List<Endereco> enderecoDTOS){
+        List<EnderecoDTO> enderecos = new ArrayList<>();
+        for(Endereco enderecoDTO : enderecoDTOS){ // um for para que transforme a funcao a baixo em uma lista
+            enderecos.add(paraEnderecoDTO(enderecoDTO));
+        }
+        return enderecos;
+    }
+
+    public EnderecoDTO paraEnderecoDTO(Endereco enderecoDTO){
+        return EnderecoDTO.builder()
+                .rua(enderecoDTO.getRua())
+                .numero(enderecoDTO.getNumero())
+                .cidade(enderecoDTO.getCidade())
+                .complemento(enderecoDTO.getComplemento())
+                .cep(enderecoDTO.getCep())
+                .estado(enderecoDTO.getEstado())
+                .build();
+    }
+
+    public List<TelefoneDTO> paraListaTelefonesDTO(List<Telefone> telefoneDTOS){
+        return telefoneDTOS.stream().map(this::paraTelefoneDTO).toList(); //.stream.map faz o mesmo que o for, pegando cada item do telefone e retornando, ja o .toList pega esse retorno dos itens e joga na paraListTelefone como lista
+    }
+
+    public TelefoneDTO paraTelefoneDTO(Telefone telefoneDTO){
+        return TelefoneDTO.builder()
+                .numero(telefoneDTO.getNumero())
+                .ddd(telefoneDTO.getDdd())
+                .build();
+    }
+
+}
